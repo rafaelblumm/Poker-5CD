8ba766f906e879e1eb71e09ccd8d5278d461b1bf
Rafael Blumm <109688389+rafaelblumm@users.noreply.github.com>
2022-11-22 19:38:18 -0300

    Class implements Comparable & Additional methods
    Added compareTo(), imprimeCombinacao(), nomeCombinacao().

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,14 +1,15 @@
 // RAFAEL FLORES BLUMM
 
-public class Jogador {
+public class Jogador implements Comparable<Jogador>{
 	private String nome;
-	private int fichas;
+	private int fichas, pontuacao;
 	private Carta[] mao;
 	private boolean bot, jogandoRodada;
 	
 	public Jogador(String nome, boolean bot) {
 		this.nome = nome;
 		this.fichas = 200;
+		this.pontuacao = 0;
 		this.mao = new Carta[5];
 		this.bot = bot;
 		this.jogandoRodada = false;
@@ -28,6 +29,14 @@ public class Jogador {
 
 	public void setFichas(int fichas) {
 		this.fichas = fichas;
+	}
+
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
 	}
 
 	public Carta[] getMao() {
@@ -59,9 +68,60 @@ public class Jogador {
 		return "É bot: "+bot+"\nNome: "+nome+"\nFichas: $"+fichas;
 	}
 	
+	@Override
+	public int compareTo(Jogador j) {
+		return pontuacao - j.getPontuacao();
+	}
+	
 	public void imprimeMao() {
 		for(int i = 0; i < mao.length; i++)
 			System.out.print(mao[i].cartaSimbolo() + (i == mao.length-1 ? "\n" : " - "));
+	}
+	
+	public void imprimeCombinacao() {
+		if(pontuacao == 10)
+			System.out.println("Você tem um ROYAL-FLUSH.");
+		else if(pontuacao == 9)
+			System.out.println("Você tem um STRAIGHT-FLUSH.");
+		else if(pontuacao == 8)
+			System.out.println("Você tem uma QUADRA.");
+		else if(pontuacao == 7)
+			System.out.println("Você tem um FULL-HOUSE.");
+		else if(pontuacao == 6)
+			System.out.println("Você tem um FLUSH.");
+		else if(pontuacao == 5)
+			System.out.println("Você tem um STRAIGHT.");
+		else if(pontuacao == 4)
+			System.out.println("Você tem uma TRINCA de "+mao[2].getNome()+".");
+		else if(pontuacao == 3)
+			System.out.println("Você tem DOIS PARES.");
+		else if(pontuacao == 2)
+			System.out.println("Você tem UM PAR.");
+		else
+			System.out.println("Você tem uma HIGH CARD: "+mao[mao.length - 1].toString()+".");
+	}
+	
+	public String nomeCombinacao() {
+		if(pontuacao == 10)
+			return "ROYAL-FLUSH";
+		else if(pontuacao == 9)
+			return "STRAIGHT-FLUSH";
+		else if(pontuacao == 8)
+			return "QUADRA";
+		else if(pontuacao == 7)
+			return "FULL-HOUSE";
+		else if(pontuacao == 6)
+			return "FLUSH";
+		else if(pontuacao == 5)
+			return "STRAIGHT";
+		else if(pontuacao == 4)
+			return "TRINCA";
+		else if(pontuacao == 3)
+			return "DOIS PARES";
+		else if(pontuacao == 2)
+			return "UM PAR";
+		else
+			return "HIGH CARD";
 	}
 	
 	public void recebeCarta(Carta c, int pos) {
