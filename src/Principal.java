8d3aeea2d75e74bd1e032644128e160ab52582b0
Rafael Blumm <109688389+rafaelblumm@users.noreply.github.com>
2022-11-23 13:58:00 -0300

    Implemented basic game dynamic

--- src/Principal.java
+++ src/Principal.java
@@ -4,39 +4,42 @@ public class Principal {
 	public static void main(String[] args) {
 		Controle ctrl = new Controle();
 		ctrl.adicionaJogadores();
-		ctrl.imprimeJogadores();
 		
-		//while(ctrl.getJogadores().size() > 1) {}
-		ctrl.novaRodada();
-		System.out.println("RODADA Nº "+ctrl.getRodada());
+		while(ctrl.getJogadores().size() > 1) {
+			ctrl.novaRodada();
+			
+			
+			// ETAPA 1: DISTRIBUIÇÃO DAS CARTAS
+			System.out.println("\nDISTRIBUIÇÃO DAS CARTAS");
+			ctrl.distribuiMaos();
+			System.out.println(ctrl.getJogadorDealer().getNome()+" está na posição do DEALER.");
+			
+			
+			// ETAPA 2: APOSTAS INICIAIS
+			System.out.println("\nAPOSTAS INICIAIS");
+			ctrl.apostasIniciais();
+			System.out.println("SMALL BLIND | "+ctrl.getJogadorSB().getNome()+": +$"+ctrl.getSmallBlind()+
+								"\nBIG BLIND   | "+ctrl.getJogadorBB().getNome()+": +$"+ctrl.getBlind()+
+								"\n> Total de fichas no pote: $"+ctrl.getPote());
+			
+			
+			// ETAPA 3: TROCA DE CARTAS
+			System.out.println("\nTROCA DE CARTAS");
+			ctrl.rodadaTrocaCartas();
+			
+			
+			// ETAPA 4: APOSTAS
+			System.out.println("\nRODADA DE APOSTAS");
+			ctrl.rodadaApostas();
+			System.out.println("\nFIM DA RODADA DE APOSTAS");
+			
+			
+			// ETAPA 5: RESULTADO
+			ctrl.rodadaResultado();
 		
+		}
 		
-		// ETAPA 1: DISTRIBUIÇÃO DAS CARTAS
-		System.out.println("\nDISTRIBUIÇÃO DAS CARTAS");
-		ctrl.distribuiMaos();
-		
-		
-		// ETAPA 2: APOSTAS INICIAIS
-		System.out.println("\nAPOSTAS INICIAIS");
-		ctrl.apostasIniciais();
-		System.out.println("SMALL BLIND | "+ctrl.getJogadorSB().getNome()+": +$"+ctrl.getSmallBlind()+
-							"\nBIG BLIND   | "+ctrl.getJogadorBB().getNome()+": +$"+ctrl.getBlind()+
-							"\n> Total de fichas no pote: $"+ctrl.getPote());
-		
-		
-		// ETAPA 3: TROCA DE CARTAS
-		System.out.println("\nTROCA DE CARTAS");
-		ctrl.rodadaTrocaCartas();
-		
-		
-		// ETAPA 4: APOSTAS
-		System.out.println("\nRODADA DE APOSTAS");
-		ctrl.rodadaApostas();
-		System.out.println("\nFIM DA RODADA DE APOSTAS");
-		
-		
-		// ETAPA 5: RESULTADO
-		ctrl.rodadaResultado();
-		
+		System.out.println("***********************************************\n\n"+
+							"Parabéns, "+ctrl.getJogadores().get(0).getNome()+"! Você venceu a partida!");
 	}
 }
