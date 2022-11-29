45e80661cbb0e22db1850ee00dfba8279cb558e8
Rafael Blumm <109688389+rafaelblumm@users.noreply.github.com>
2022-11-29 18:41:44 -0300

    Added comments to methods

--- src/Baralho.java
+++ src/Baralho.java
@@ -2,7 +2,7 @@
 
 public class Baralho {
 	private Carta[] cartas;
-	
+
 	public Baralho() {
 		cartas = new Carta[52];
 		String[] nomeCartas = {"Dois", "Três", "Quatro", "Cinco", "Seis", "Sete",
@@ -25,12 +25,15 @@ public class Baralho {
 		this.cartas = cartas;
 	}
 	
+	/**
+	 * Método que embaralha cartas de maneira aleatória.
+	 */
 	public void embaralha() {
 		int valorAleatorioA, valorAleatorioB;
 		Carta holdCarta;
 		for(int i = 0; i < 200; i++) {
-			valorAleatorioA = (int)(Math.random() * 52);          // Math.random() * (max - min) + min
-			valorAleatorioB = (int)(Math.random() * 52);          // Math.random() * (52 - 0) + 0
+			valorAleatorioA = (int)(Math.random() * 52);
+			valorAleatorioB = (int)(Math.random() * 52);
 			holdCarta = cartas[valorAleatorioB];
 			
 			cartas[valorAleatorioB] = cartas[valorAleatorioA];
@@ -38,6 +41,10 @@ public class Baralho {
 		}
 	}
 	
+	/**
+	 * Método que remove e retorna a carta do topo do baralho.
+	 * @return (Carta) carta do topo do baralho.
+	 */
 	public Carta distribuiCarta() {
 		Carta holdCarta;
 		int i = 0;
@@ -51,6 +58,10 @@ public class Baralho {
 		}
 	}
 	
+	/**
+	 * Método que recebe um objeto Carta por parâmetro e a insere no Array de Carta.
+	 * @param (Carta) carta a ser inserida no Array de Carta.
+	 */
 	public void adicionaCarta(Carta c) {
 		for(int i = 0; i < cartas.length; i++)
 			if(cartas[i] == null) {
@@ -59,6 +70,10 @@ public class Baralho {
 			}
 	}
 	
+	/**
+	 * Método que conta quantos objetos de Carta existem no Array de Carta.
+	 * @return (int) quantidade de cartas no baralho.
+	 */
 	public int quantCartas() {
 		int total = 0;
 		for(Carta c: cartas)
@@ -67,6 +82,9 @@ public class Baralho {
 		return total;
 	}
 	
+	/**
+	 * Método que imprime o baralho carta por carta.
+	 */
 	public void imprimeBaralho() {
 		for(Carta c: cartas)
 			if(c != null)
